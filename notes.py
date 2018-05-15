8b7cd3363450a92c6fe1875e9d17e38d4347783d
hirochri <hirochri@umich.edu>
2018-05-15 16:48:51 -0400

    Custom comparator notes

---
+++ notes.py
@@ -0,0 +1,6 @@
+
+#Python can do custom sorting with comparator
+#Need a func that returns a negative number for less than,
+#0 for equality or a positive number for greater than
+#Then import cmp_to_key from functools and pass that
+#to the key of the sort: x.sort(key=functools.cmp_to_key(comparator_func))
